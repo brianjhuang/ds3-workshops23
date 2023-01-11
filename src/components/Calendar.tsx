01059315ac24078f21f6e3bcfde4873211a442bb
brianjhuang <brianjhuang25@gmail.com>
2023-01-11 12:42:51 -0800

    Registration form

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,12 +12,16 @@ import UnderConstruction from "./UnderConstruction";
 
 function Calendar() {
   return (
-    <iframe
-      src="https://calendar.google.com/calendar/embed?src=c_6a154893689d6a0906f6311d88adf4ca5c125aa6de0dc09e95375593071ff40b%40group.calendar.google.com&ctz=America%2FLos_Angeles"
-      width="800"
-      height="600"
-      scrolling="no"
-    ></iframe>
+    <>
+      {" "}
+      <h3>Register for winter workshops <a href = "https://oq2voqd6dmx.typeform.com/to/IUBZn0O7">here!</a></h3>
+      <iframe
+        src="https://calendar.google.com/calendar/embed?src=c_6a154893689d6a0906f6311d88adf4ca5c125aa6de0dc09e95375593071ff40b%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+        width="800"
+        height="600"
+        scrolling="no"
+      ></iframe>
+    </>
   );
 }
 
